e7a7ca09527987b09a27475855e445a67bc3ab62
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-06-28 14:08:10 +0000

    basic container

--- build.sh
+++ build.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-osmium extract --bbox=77.666931,27.557615,77.723904,27.590101 --set-bounds --strategy=smart ./india-latest.osm.pbf  --output ./vrindavan/vrindavan.osm.pbf
+
+# Install dependencides
+git submodule update --init --recursive
+cd ./tilemaker && make install
+
+osmium extract --bbox=77.666931,27.557615,77.723904,27.590101 --set-bounds --strategy=smart ./india-latest.osm.pbf  --output ./output/vrindavan.osm.pbf
+./tilemaker --input ./output/vrindavan.osm.pbf --outpuin .mbtiles --process ./configs/tilemaker.lua --config ./configs/tilemaker.json
