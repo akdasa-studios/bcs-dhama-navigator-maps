cb5faeacc7a2da1d1a3a6a48b1c52f34434370f3
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-06-28 15:09:14 +0000

    tilemaker

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-osmium extract --bbox=77.666931,27.557615,77.723904,27.590101 --set-bounds --strategy=smart ./india-latest.osm.pbf  --output ./output/vrindavan.osm.pbf
-/usr/local/bin/tilemaker --input ./output/vrindavan.osm.pbf --outpuin .mbtiles --process ./configs/tilemaker.lua --config ./configs/tilemaker.json
+osmium extract --bbox=77.666931,27.557615,77.723904,27.590101 --set-bounds --strategy=smart ./india-latest.osm.pbf  --output ./output/vrindavan.osm.pbf --overwrite
+/usr/local/bin/tilemaker --input ./output/vrindavan.osm.pbf --output ./output/vrindavan.mbtiles --process ./configs/tilemaker.lua --config ./configs/tilemaker.json
+/usr/local/bin/tilemaker --input ./output/vrindavan.osm.pbf --output ./output/vrindavan --process ./configs/tilemaker.lua --config ./configs/tilemaker.json
+
