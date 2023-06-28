4be311e2574059405b17e57209f332824bb80dc0
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-06-28 14:11:20 +0000

    update pipeline

--- build.sh
+++ build.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
-# Install dependencides
-git submodule update --init --recursive
-cd ./tilemaker && make install
-
 osmium extract --bbox=77.666931,27.557615,77.723904,27.590101 --set-bounds --strategy=smart ./india-latest.osm.pbf  --output ./output/vrindavan.osm.pbf
-./tilemaker --input ./output/vrindavan.osm.pbf --outpuin .mbtiles --process ./configs/tilemaker.lua --config ./configs/tilemaker.json
+/usr/local/bin/tilemaker --input ./output/vrindavan.osm.pbf --outpuin .mbtiles --process ./configs/tilemaker.lua --config ./configs/tilemaker.json
